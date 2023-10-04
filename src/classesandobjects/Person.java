3d83afea5746eaa44660b7d574b34812072e487b
BHDisrael <israel970104@gmail.com>
2023-10-04 13:11:16 +0100

    Main person class

---
+++ src/classesandobjects/Person.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjects;
+
+import shapes.Triangle;
+
+/**
+ *
+ * @author aamyr
+ */
+public class Person {
+    
+    double height;
+    int age;
+    String name;
+    boolean isProgrammer;
+    Triangle favTriangle;
+    
+}
